531cb9efbd48e55cd5eb93e43907b937350901a9
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-11-24 19:43:58 +0530

    Create App.js

---
+++ _frontend/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Footer from "./Routes/Footer";
+import Navbar from "./Routes/Navbar";
+import Router from "./Routes/Router";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Router />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
